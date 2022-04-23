c2be0f773c2d811fcc51ef76a88cde81622ec487
DarkNoriss <darknoriss@gmail.com>
2022-04-23 20:57:00 +0200

    urgay

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ namespace Tutorial {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
