e51656cbe7f39e2782a30e089ab2b313d9478e4e
DarkNoriss <darknoriss@gmail.com>
2022-04-23 20:58:52 +0200

    best one yet

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@ namespace Tutorial {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("hahaugay!");
         }
     }
 }
