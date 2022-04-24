b5e36ad145100b2a63f74545b2aab9e9e1ff8f46
DarkNoriss <darknoriss@gmail.com>
2022-04-24 02:03:10 +0200

    added some new stuff :^)

--- Program.cs
+++ Program.cs
@@ -3,8 +3,25 @@
 namespace Tutorial {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("hahaugay!");
+            Console.Title = "FirstProgram";
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (OperatingSystem.IsWindows()) {
+                Console.WindowHeight = 40;
+            }
+
+            Console.WriteLine("Yo whats ur name dud?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("I am GOD \nand im better than u!");
+            Console.WriteLine("Whats ur fav animal?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Cool but who asked?");
+
+            Console.ReadKey();
+
         }
     }
 }
