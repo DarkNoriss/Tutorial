68dda5e113a75615cdf04bae707230647c90b8fd
DarkNoriss <darknoriss@gmail.com>
2022-04-25 22:28:28 +0200

    again i was doing on master stage i have stupidy

--- Program.cs
+++ Program.cs
@@ -6,26 +6,42 @@ namespace Tutorial {
         static void Main(string[] args) {
             Console.Title = "FirstProgram";
             Console.ForegroundColor = ConsoleColor.Green;
+
+            int userAge;
+            string userName, userAnimal;
+
             if (OperatingSystem.IsWindows()) {
                 Console.WindowHeight = 40;
             }
+
             Console.WriteLine("Yo whats ur name dud?");
-            string nameUser = Console.ReadLine();
-            if (nameUser.Any(char.IsDigit)) {
+            userName = Console.ReadLine();
+
+            if (userName.Any(char.IsDigit)) {
                 Console.WriteLine("Thats not a real name dud...");
             }
+
             Console.WriteLine("I am GOD and im better than u!");
+            Console.WriteLine("Whats ur age?");
+            userAge = Convert.ToInt32(Console.ReadLine());
+
+            if (userAge <= 18) {
+                Console.WriteLine("lol not even an adult KEKW");
+            }
+
             Console.WriteLine("Whats ur fav animal?");
-            string nameAnimal = Console.ReadLine();
-            if (nameUser.Any(char.IsDigit)) {
+            userAnimal = Console.ReadLine();
+
+            if (userAnimal.Any(char.IsDigit)) {
                 Console.WriteLine("Thats not a real animal dud...");
             } else {
-                if (nameAnimal.Length > 5) {
+                if (userAnimal.Length > 5) {
                     Console.WriteLine("Cool but who asked?");
                 } else {
                     Console.WriteLine("Thats nice");
                 }
             }
+
             Console.ReadKey();
         }
     }
